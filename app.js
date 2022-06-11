ebfa9c3f0e9848ad41b39295ff9390738874d6bc
Thaynara <contato.tainararodrigues@gmail.com>
2022-06-11 13:09:35 -0300

    logs with morgan

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const express = require('express')
 const app = express()
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+var morgan = require('morgan')
 const cors = require('cors');
 const usersRoute = require('./routes');
 const port = process.env.PORT
+const fs = require('fs')
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
+const accessLogStream = fs.createWriteStream(__dirname + '/http.log', {flags: 'a'})
+app.use(morgan('combined', {"stream": accessLogStream}))
 
 app.use('/api',usersRoute)
 const server = app
